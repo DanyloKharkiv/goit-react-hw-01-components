afbab413cc2441775751b723d89e950a9cf54f38
Danylo Kharkiv <donatelod12@gmail.com>
2023-02-21 20:33:01 +0200

    add friend list task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,15 @@ import user from '../database/user.json';
 import Statistics from './Statistics/Statistics';
 import data from '../database/data.json';
 
+import FriendList from './FriendList/FriendList';
+import dataFriends from '../database/friends.json';
+
 export const App = () => {
   return (
     <>
       < Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
       < Statistics title="Upload stats" stats={data} />
+      < FriendList friends={dataFriends} />
     </>
   );
 };
