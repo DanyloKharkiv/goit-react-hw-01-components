587aa10d8847f28d472b93759a155c619f17379a
Danylo Kharkiv <donatelod12@gmail.com>
2023-02-21 20:37:55 +0200

    add transaction history task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,16 @@ import data from '../database/data.json';
 import FriendList from './FriendList/FriendList';
 import dataFriends from '../database/friends.json';
 
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../database/transactions.json';
+
 export const App = () => {
   return (
     <>
       < Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
       < Statistics title="Upload stats" stats={data} />
       < FriendList friends={dataFriends} />
+      < TransactionHistory items={transactions} />
     </>
   );
 };
