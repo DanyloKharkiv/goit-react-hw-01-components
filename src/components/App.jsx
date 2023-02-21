ca49b69392e19e3abe8b1c4e8710d12add1d2eb7
Danylo Kharkiv <donatelod12@gmail.com>
2023-02-21 20:28:58 +0200

    add statistic task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import Profile from './Profile/Profile';
 import user from '../database/user.json';
 
+import Statistics from './Statistics/Statistics';
+import data from '../database/data.json';
+
 export const App = () => {
   return (
     <>
       < Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      < Statistics title="Upload stats" stats={data} />
     </>
   );
 };
